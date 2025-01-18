46d1c7b568b7e6daf2afc182ebe43e84de021768
shihab20105 <shihabur10@gmail.com>
2025-01-18 21:53:31 +0000

    Completed Question 5

--- 24-1 Basic JS Problem Solving assignment/assignment.js
+++ 24-1 Basic JS Problem Solving assignment/assignment.js
@@ -1,1 +1,64 @@
 /*                          24-1 Basic Javascript Problem Solving assignment */
+
+/* 
+** Question 1: Convert radian to degree. You need to write a function where I give a number as the amount of radians, and you return how many degrees are in 5 radians, Return only the number. You must name the function exactly as it is written here.
+'radianToDegree'
+
+** Question 2: You have to check, I will give you a file name, you tell me whether it is a JavaScript file or not. You must name the function exactly as it is written here.
+'isJavaScriptFile'
+
+    Examples:
+    script.js
+    app.min.js
+    main.module.js
+
+    index.html
+    style.css
+    data.json
+    image.png
+
+** Question 3: Calculate the total oil price that I have to pay.
+    diesel per litre 114
+    petrol per litre 130
+    octane per litre 135
+
+    The first parameter will be how much diesel I want to take. For the first parameter I want 15 litre.
+    The second parameter will be how much petrol I want to take. For the second parameter I want 10 litre.
+    The third parameter will be how much octane I want to take. For the third  parameter I want 8 litre.
+How much will I have to pay in total? Return that to me in a number. You must name the function exactly as it is written here. 
+    'oilPrice'
+
+** Question 4: For example, I am going on a picnic. I can use three types of transportation. How much do I have to pay for public bus? You must name the function exactly as it is written here.
+
+    Reserved bus capacity 50
+    Reserved microbus capacity 11
+    Rest will go by public bus
+
+Total people 65
+    Reserved bus will take 50 (With the bus i will take 50, even not less than 50. That means i will take first 50.)
+    People remaining 15
+    Microbus will take 4
+    Public bus will take rest 
+
+Public bus fare 250
+
+You must name the function exactly as it is written here.
+    'publicBusFare'
+
+** Question 5: 'isBestFriend' For this i will take two parameters. And as parameter i wil take two objects and every object must have at least two properties.
+    {name: 'Tom', friend: 'Rock'}   // First parameter
+    {name: 'Rock', friend: 'Tom'}   // Second parameter
+
+What do you have to check, if the first object i gave that one 'name' property if second objects 'friend' property value are same, and the first object we have that one 'friend' name, and the second object we have that one 'name', if both are similar then we will call them friend. 
+    That means Tom saying Rock his friend
+    And Rock saying Tom his friend.
+
+And they will became friend. 
+
+    {name: 'Chris', friend: 'John'} 
+    {name: 'Roy', friend: 'Chris'}
+
+You have to return true or false?
+You must name the function exactly as it is written here.
+    'isBestFriend'
+*/
